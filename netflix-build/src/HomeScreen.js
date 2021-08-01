ef0a5390e896563460ac15ed01d0b729bdb71348
ZaydenBlaze14 <peeyushyadav1402@gmail.com>
2021-08-01 12:52:53 +0530

    Initial commit

---
+++ netflix-build/src/HomeScreen.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+import Banner from './Banner';
+import requests from './requests';
+import Row from './Row';
+
+function HomeScreen() {
+    return (
+        <div className='homeScreen'>
+            <Nav/>
+            
+            <Banner/>
+
+            <Row 
+                title = 'Netflix Originals'       
+                fetchURL = {requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+
+            <Row 
+                title = 'Trending Now'       
+                fetchURL = {requests.fetchTrending}
+            />
+
+            <Row 
+                title = 'Action Movies '       
+                fetchURL = {requests.fetchActionMovies}
+            />
+
+            <Row 
+                title = 'Comedy Movies'       
+                fetchURL = {requests.fetchComedyMovies}
+            />
+
+            <Row 
+                title = 'Documentaries '       
+                fetchURL = {requests.fetchDocumentaries}
+                />
+
+            <Row 
+                title = 'Horror Movies'       
+                fetchURL = {requests.fetchHorrorMovies}
+                />
+
+            <Row 
+                title = 'Romance Movies '       
+                fetchURL = {requests.fetchRomanceMovies}
+                />
+
+            <Row 
+                title = 'Top Rated'       
+                fetchURL = {requests.fetchTopRated}
+                />
+
+
+        </div>
+        );
+}
+
+export default HomeScreen
---
+++ netflix-build/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1u-P9QQ1tR5LTsgrGRqZk0oQhBM12nOo",
+    authDomain: "netflix-clone-8deeb.firebaseapp.com",
+    projectId: "netflix-clone-8deeb",
+    storageBucket: "netflix-clone-8deeb.appspot.com",
+    messagingSenderId: "604791473430",
+    appId: "1:604791473430:web:96c3bdd63b3f77b4a5bc37"
+  };
