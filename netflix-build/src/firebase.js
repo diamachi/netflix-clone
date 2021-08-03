722189ac4f633d24ba16f73d62945bb9b93e0538
ZaydenBlaze14 <peeyushyadav1402@gmail.com>
2021-08-03 23:01:43 +0530

    Ui Part 2

--- netflix-build/src/firebase.js
+++ netflix-build/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB1u-P9QQ1tR5LTsgrGRqZk0oQhBM12nOo",
     authDomain: "netflix-clone-8deeb.firebaseapp.com",
@@ -5,4 +7,13 @@ const firebaseConfig = {
     storageBucket: "netflix-clone-8deeb.appspot.com",
     messagingSenderId: "604791473430",
     appId: "1:604791473430:web:96c3bdd63b3f77b4a5bc37"
-  };
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export {auth}
+export default db;
