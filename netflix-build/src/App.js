0d3895a018b8cf2edc2eebb1709b8f7b0758bec0
zaydenblaze <peeyushyadav1402@gmail.com>
2021-08-04 16:29:20 +0530

    Completed signin functionality along with added persistence

--- netflix-build/src/App.js
+++ netflix-build/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {
@@ -7,10 +7,33 @@ import {
   Route
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import {useDispatch, useSelector} from "react-redux";
+import { login, logout, selectUser } from './features/userSlice';
 
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth){
+        //logged in
+        dispatch(login({
+          uid:userAuth.uid,
+          email:userAuth.email,
+        })
+        );
+      } else {
+        //logged out
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  },[]);
+
   return (
     <div className="app">
       
@@ -21,6 +44,9 @@ function App() {
           <LoginScreen/>
         ) : (
           <Switch>
+            <Route path='/profile'>
+              <ProfileScreen/>
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
--- netflix-build/src/screens/SignupScreen.js
+++ netflix-build/src/screens/SignupScreen.js
@@ -1,22 +1,43 @@
-import React from 'react'
-import "./SignupScreen.css"
+import React, { useRef } from 'react';
+import "./SignupScreen.css";
+import {auth} from "../firebase";
 
 function SignupScreen() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
 
     const register = (e) => {
         e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) =>{
+            console.log(authUser);
+        }).catch(error => {
+            alert(error.message);
+        });
     };
 
     const signIn = (e) => {
         e.preventDefault();
-    }
+
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser);
+        })
+        .catch((error) => alert(error.message));
+    };
 
     return (
         <div className="signupScreen">
             <form>
                 <h1>Sign In</h1>
-                <input placeholder="E-Mail" type="email"/>
-                <input placeholder="Password" type="password"/>
+                <input ref = {emailRef} placeholder="E-Mail" type="email"/>
+                <input ref = {passwordRef} placeholder="Password" type="password"/>
                 <button type="submit" onClick={signIn}>Sign In</button>
                 <h4>
                 <span className="signupScreen__gray">New to Netflix? </span> 
@@ -26,4 +47,4 @@ function SignupScreen() {
     )
 }
 
-export default SignupScreen
+export default SignupScreen;
